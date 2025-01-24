99a8079e2e84937938661aa70d3f837ee2530887
yuhao <yuhao.wu@mail.mcgill.ca>
2025-01-24 07:07:58 -0800

    [FEATURE]: Modify generate_usd.py so that it works on sockeye

--- generate_usd.py
+++ generate_usd.py
@@ -4,6 +4,8 @@ from warp import vec3
 import pickle
 import numpy as np
 from typing import Dict, List, Tuple
+import argparse
+import os
 
 
 def render_usd_per_frame(
@@ -35,54 +37,70 @@ def render_usd_per_frame(
                 f"particle_{particle_idx:03d}_gt",
                 xpos_gt[particle_idx],
                 (0.0, 0.0, 0.0, 1.0),
-                0.01, # self.ball_radius
+                0.2, # self.ball_radius
                 color=(0.2, 0.4, 0.6))
             renderer.render_sphere(
                 f"particle_{particle_idx:03d}_pred",
                 xpos_pred[particle_idx],
                 (0.0, 0.0, 0.0, 1.0),
-                0.01, # self.ball_radius
+                0.2, # self.ball_radius
                 color=(0.6, 0.2, 0.2))
         else:
             renderer.render_sphere(
                 f"particle_{particle_idx:03d}_fixed",
                 xpos_gt[particle_idx],
                 (0.0, 0.0, 0.0, 1.0),
-                0.01, # self.ball_radius
+                0.2, # self.ball_radius
                 color=(0.0, 0.0, 0.0))
     
     renderer.end_frame()
     return
 
-# read into particle_data
-pickle_file_path = "eval_vis/fem/iter_300000-FEM-test/0.pkl"
-with open(pickle_file_path, 'rb') as f:
-    pickle_data = pickle.load(f)
 
-predicted_positions = pickle_data['pred_poss']
-target_positions = pickle_data['tgt_poss']
-nonk_mask = pickle_data['nonk_mask']
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dir_path", type=str, required=True, help="The name of directory containing the pickle files")
+    parser.add_argument("--pickle_idx", type=str, required=True, help="The index of the pickle file")
+    args = parser.parse_args()
+    
+    dir_path = args.dir_path
+    pickle_idx = args.pickle_idx
+    pickle_file_name = f"{pickle_idx}.pkl"
+    
+    pickle_file_path = os.path.join(dir_path, pickle_file_name)
+    
+    # read into particle_data
+    with open(pickle_file_path, 'rb') as f:
+        pickle_data = pickle.load(f)
 
-# Set up the WARP renderer
-stage = "animation.usd"
-renderer = render.UsdRenderer(stage=stage)
+    predicted_positions = pickle_data['pred_poss']
+    target_positions = pickle_data['tgt_poss']
+    nonk_mask = pickle_data['nonk_mask']
 
-# render
-num_frames = predicted_positions.shape[0]
+    # Set up the WARP renderer
+    stage_name = f"{pickle_idx}.usd"
+    stage = os.path.join(dir_path, stage_name)
+    renderer = render.UsdRenderer(stage=stage)
 
-sim_dt = 0.001
+    # render
+    num_frames = predicted_positions.shape[0]
 
-for frame_idx in range(num_frames):
-    target_positions_frame = target_positions[frame_idx]
-    predicted_positions_frame = predicted_positions[frame_idx]
-    sim_time = frame_idx * sim_dt
-    render_usd_per_frame(
-        renderer,
-        target_positions_frame,
-        predicted_positions_frame,
-        nonk_mask,
-        sim_time)
+    sim_dt = 0.001
 
-# save
-renderer.save()
-print(f"USD file saved to {stage}")
+    for frame_idx in range(num_frames):
+        target_positions_frame = target_positions[frame_idx]
+        predicted_positions_frame = predicted_positions[frame_idx]
+        sim_time = frame_idx * sim_dt
+        render_usd_per_frame(
+            renderer,
+            target_positions_frame,
+            predicted_positions_frame,
+            nonk_mask,
+            sim_time)
+
+    # save
+    renderer.save()
+    print(f"USD file saved to {stage}")
+
+if __name__ == "__main__":
+    main()
